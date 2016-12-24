b2b524731f8dc3651f69d978b27b59eb3f13fe97
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2016-12-24 21:44:21 +0600

    Initial commit

---
+++ app/src/main/java/com/pykod/webtoapp/App.java
@@ -0,0 +1,15 @@
+package com.pykod.webtoapp;
+
+import android.app.Application; 
+import com.parse.Parse; 
+import com.parse.ParsePush;
+
+public class App extends Application { 
+
+    @Override public void onCreate() { 
+        super.onCreate();
+
+		Parse.initialize(this, "", ""); 
+        ParsePush.subscribeInBackground("");
+    }
+} 
